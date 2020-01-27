86a93fc267c2867279195cf5f2302aba3584df57
eshaamrabaney <eshaam.rabaney@gmail.com>
2020-01-27 21:54:17 +0200

    init

---
+++ server/build/precache-manifest.4c0676f957be659743357dc3f9aff4dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca575c6d27fe6e7b1ebe5f7379b68d63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56adfa3285848d9f23ff",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ef042697e3743010768c",
+    "url": "/static/js/2.1381f5e2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.1381f5e2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "56adfa3285848d9f23ff",
+    "url": "/static/js/main.14f337a0.chunk.js"
+  },
+  {
+    "revision": "73e4e604d24fdcb3dadd",
+    "url": "/static/js/runtime-main.5b182252.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
